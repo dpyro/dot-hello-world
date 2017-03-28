517b1f1b650f88a0ad4e51a99bd54d8be0b3303f
Sumant Manne <sumant.manne@gmail.com>
2017-03-27 20:58:19 -0500

    Added Gulp, ESLint

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+const gulp = require('gulp');
+const eslint = require('gulp-eslint');
+
+gulp.task('lint', () => {
+  return gulp
+    .src(['**/*.js', '!node_modules/**'])
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
+});
+
+gulp.task('default', ['lint'], () => {
+  // This will only run if the lint task is successful...
+});
